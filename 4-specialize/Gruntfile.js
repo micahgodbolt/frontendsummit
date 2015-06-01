ae91ce17f974f4669aba199f763f59957df469e9
Micah Godbolt <mgodbolt@phase2technology.com>
2015-05-31 22:12:02 -0700

    updated folder name

--- 4-specialize/Gruntfile.js
+++ 4-specialize/Gruntfile.js
@@ -16,19 +16,19 @@ module.exports = function(grunt) {
   // Configure Tasks
   ////////////////////////
 
-  require('./grunt-tasks/sass.js')(grunt, config);
+  require('./grunt_tasks/sass.js')(grunt, config);
 
-  require('./grunt-tasks/javascript.js')(grunt, config);
+  require('./grunt_tasks/javascript.js')(grunt, config);
 
   ////////////////////////
   // Register New Tasks
   ////////////////////////
 
-  require('./grunt-tasks/notSoAwesome.js')(grunt, config);
+  require('./grunt_tasks/notSoAwesome.js')(grunt, config);
 
-  require('./grunt-tasks/awesome.js')(grunt, config);
+  require('./grunt_tasks/awesome.js')(grunt, config);
 
-  require('./grunt-tasks/moreAwesome.js')(grunt, config);
+  require('./grunt_tasks/moreAwesome.js')(grunt, config);
 
 
 
